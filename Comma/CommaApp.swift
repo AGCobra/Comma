6c974ec983379afe89b8b73e034f0a7c55353348
agcobra <agchehebar@gmail.com>
2025-12-08 15:44:17 -0500

    Initial commit - Comma Xcode project

---
+++ Comma/CommaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommaApp.swift
+//  Comma
+//
+//  Created by Albert Chehebar on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CommaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommaTests/CommaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommaTests.swift
+//  CommaTests
+//
+//  Created by Albert Chehebar on 12/8/25.
+//
+
+import Testing
+@testable import Comma
+
+struct CommaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
